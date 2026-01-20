617aca059639e4ef82989382627331b41848195a
rohit <surverohita32@gmail.com>
2026-01-21 00:10:22 +0530

    Docker set up for rabbit mq for all the services

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -5,6 +5,9 @@ import com.fitness.activityservice.dto.ActivityRequest;
 import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.model.Activity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,9 +15,23 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityService {
     private final  ActivityRepository activityRepository;
+    private final UserValidationService userValidationService;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
     public ActivityResponse trackActivity(ActivityRequest request) {
+        boolean isValidUser=userValidationService.validateUser(request.getUserId());
+        if(!isValidUser){
+            throw new RuntimeException("Invalid User"+request.getUserId());
+        }
+
         Activity activity= Activity.builder()
                 .userId(request.getUserId())
                 .type(request.getType())
@@ -23,7 +40,18 @@ public class ActivityService {
                 .startTime(request.getStartTime())
                 .additionalMetrics(request.getAdditionalMetrics())
                 .build();
+
+
         Activity savedActivity=activityRepository.save(activity);
+        //publish to RabbitMQ for AI Processing
+        try{
+            rabbitTemplate.convertAndSend(exchange,routingKey,savedActivity);
+
+
+        }catch (Exception e){
+            log.error("Failed to publish activity to RabbitMQ:",e);
+
+        }
          return mapToResponse(savedActivity);
     }
     private ActivityResponse mapToResponse(Activity activity){
--- activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -1,4 +1,35 @@
 package com.fitness.activityservice.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class UserValidationService {
+    private final WebClient userServiceWebClient;
+
+    public boolean validateUser(String userId) {
+        log.info("Calling user Validation API for userId: {}",userId);
+        try {
+            return userServiceWebClient.get()
+                    .uri("/api/users/{userId}/validate", userId)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block();
+
+        } catch (WebClientResponseException e) {
+            if (e.getStatusCode().value() == 404)
+                throw new RuntimeException("User Not Found:" + userId);
+            else if (e.getStatusCode().value() == 400)
+                throw new RuntimeException("Invalid Request:" + userId);
+
+
+
+        }return false;
+    }
 }
