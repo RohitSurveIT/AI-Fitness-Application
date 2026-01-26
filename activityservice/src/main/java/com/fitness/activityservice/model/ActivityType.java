2b20fd82828ddeb85c6c523fbdc5dc67c15f168a
rohit <surverohita32@gmail.com>
2026-01-26 16:34:34 +0530

    config-server setuped in the fitness application

--- activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -1,4 +1,4 @@
-package com.fitness.activityservice.model;
+package com.fitness.aiservice.model;
 
 public enum ActivityType {
     RUNNING,WALKING,CYCLING,SWIMMING,WEIGHT_TRAINING,YOGA,HITT,CARDIO,STRETCHING,OTHER
